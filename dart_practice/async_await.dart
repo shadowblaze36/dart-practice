c97f702eb110bd1ae93e06746e5ad935b02e3471
Jorge Saravia <jorgesaravia36@gmail.com>
2021-04-13 11:56:57 -0600

    Final

---
+++ dart_practice/async_await.dart
@@ -0,0 +1,12 @@
+void main() async {
+  print('Estamos a punto de pedir datos');
+  String data = await httpGet('https://api.nasa.com/aliens');
+  print(data);
+  print('Ultima línea');
+}
+
+Future<String> httpGet(String url) {
+  return Future.delayed(new Duration(seconds: 4), () {
+    return 'Hola Mundo';
+  });
+}
